4c1d98230ab9bd71f8cff8d93c15b3f7893195f2
hunteriam <hhstuy@gmail.com>
2014-05-20 13:17:32 -0400

    improved styling

--- Parse.hs
+++ Parse.hs
@@ -7,7 +7,8 @@ where
 import System.Environment
 import System.IO
 import Data.IORef
-import Data.List (sort)
+import Data.List (sort, Map)
+import qualified Data.List as DL
 
 import Control.Monad.Fix
 
@@ -28,15 +29,18 @@ cyan = (0,255,255)
 red = (255,0,0)
 green = (250,0,0.65)
 
-{- initParse :: (Matrix m) => [String] -> Output Float -> (Float,Float,Float) -> Renderable m Float
-initParse str winSize col = parseIO str (Renderable defaultArea winSize col [] []) $ identity 4 4
-	where 
-		defaultArea = Area {xRange = (0,0), yRange = (0,0)}
--}
+
+data Sequence a = Anim3D {_frames :: (Int,Int), _values :: (a,a)}
+
+readFloats src = map read . map (\(a:as) -> case a of '.' -> '0':a:as; '-' -> a:'0':as; _ -> a:as) $ src ::[Float]
+
 initParseIO :: [String] -> ScreenBuffer -> Output Float -> (Float,Float,Float) -> IO ()
 initParseIO ls sbuf out col = parseIO ls sbuf $ (initRenderable defaultArea out col :: Renderable ListMatrix Float)
 	where 
 		defaultArea = Area {xRange = (0,0), yRange = (0,0)}
+
+parseVarys :: [String] -> Map String (Sequence Float)
+parseVarys [] = fromList []
 
 parseIO :: (Show (m Float), Matrix m) => [String] -> ScreenBuffer -> Renderable m Float -> IO ()
 parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
@@ -58,13 +62,20 @@ parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
 	| w == "render-perspective-cyclops" = do
 		let 
 			(ex:ey:ez:_) = readFloats ws
-		writeIORef sbuf $ optimizeGrid $ render $ buffer {_col = green, _lineMatrix = project (ex,ey,ez) mls, _triangleMatrix = project (ex,ey,ez) $ filter (backFace [ex,ey,ez] . rows) mtri}
-		display sbuf
+		displayBuffer sbuf $ buffer {
+			_col = green, 
+			_lineMatrix = project (ex,ey,ez) mls, 
+			_triangleMatrix = project (ex,ey,ez) $ filter (backFace [ex,ey,ez] . rows) mtri
+		}
 		parseIO ls sbuf buffer
 	| w == "render-perspective-stereo" = do
 		let
 			(ex1:ey1:ez1:ex2:ey2:ez2:_) = readFloats ws
-			left = render $ buffer {_col = cyan, _lineMatrix = project (ex1,ey1,ez1) mls, _triangleMatrix = project (ex1,ey1,ez1) mls}
+			left = render $ buffer {
+				_col = cyan, 
+				_lineMatrix = projCull (ex1,ey1,ez1) mls, 
+				_triangleMatrix = projCull (ex1,ey1,ez1) mls
+			}
 			right = render $ buffer {_col = red, _lineMatrix = project (ex2,ey2,ez2) mls, _triangleMatrix = project (ex2,ey2,ez2) mls}
 			stereo = sort (left ++ right)
 		writeIORef sbuf $ optimizeGrid stereo
@@ -74,7 +85,6 @@ parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
 		let
 			(ex:ey:ez:rx:ry:rz:_) = readFloats ws
 			rotate x y z = matrixProduct (rotateX x) . matrixProduct (rotateY y) $ rotateZ z
-			cull eye = filter (backFace eye . rows)
 		renderBuf <- newIORef buffer
 		fix $ \loop -> do
 			tbuf@(Renderable _ _ tcol tedge tmls tmtri) <- readIORef renderBuf
@@ -85,7 +95,7 @@ parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
 			}
 			writeIORef sbuf $ optimizeGrid $ render $ tbuf {
 				_lineMatrix = project (ex,ey,ez) $ _lineMatrix tbuf,
-				_triangleMatrix = project (ex,ey,ez) $ cull [ex,ey,ez] $ _triangleMatrix tbuf	
+				_triangleMatrix = projCull (ex,ey,ez) $ _triangleMatrix tbuf	
 			}
 			display sbuf
 			loop
@@ -93,9 +103,12 @@ parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
 	where
 		(w:ws) = words l
 		readInts src = map read src ::[Int]
-		readFloats src = map read . map (\(a:as) -> case a of '.' -> '0':a:as; '-' -> a:'0':as; _ -> a:as) $ src ::[Float]
+		displayBuffer sbuf buf = do 
+			writeIORef sbuf $ optimizeGrid $ render buf
+			display sbuf
+		cull eye = filter (backFace eye . rows)
+		projCull eye@(ex,ey,ez) = project eye $ cull [ex,ey,ez]
 
---parseIO :: (Show (m Float), Matrix m, Integral a, Show a) => [String] -> Screen Float -> Output Float-> m Float -> [m Float] -> (a,a,a)-> IO [(a,a,(a,a,a))]
 parse :: Matrix m => String -> Renderable m Float -> Renderable m Float
 parse l buffer@(Renderable scr out col edge mls mtri)
 	--Config
@@ -114,10 +127,10 @@ parse l buffer@(Renderable scr out col edge mls mtri)
 	| w == "rotate-y" = let (deg:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (rotateY deg)}
 	| w == "rotate-z" = let (deg:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (rotateZ deg)}
 	| w == "transform" = buffer {_lineMatrix = map ((flip matrixProduct) edge) mls,_triangleMatrix = map ((flip matrixProduct) edge) mtri, _edgematrix = (identity 4 4)}
+	| w == "vary" = buffer
 	| otherwise = buffer
 	where
 		(w:ws) = words l
 		readInts src = map read src ::[Int]
-		readFloats src = map read . map (\(a:as) -> case a of '.' -> '0':a:as; '-' -> a:'0':as; _ -> a:as) $ src ::[Float]
 
 -- Functions
