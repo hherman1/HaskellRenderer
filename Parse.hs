788c62f4a9c3a646bf5dc1726793e01d9839c121
hunteriam <hhstuy@gmail.com>
2014-05-21 08:02:24 -0400

    Broken impl of Parsers, near finish

--- Parse.hs
+++ Parse.hs
@@ -17,7 +17,7 @@ import Control.Monad.Fix
 import OpenGL
 import Graphics.UI.GLUT.Window (postRedisplay)
 
-
+import Parsers
 import Matrix
 import Matrix3D
 import Objects
@@ -32,37 +32,12 @@ red = (255,0,0)
 green = (250,0,0.65)
 
 
-data Sequence a = Anim3D {_frames :: (Int,Int), _values :: (a,a)}
-
-readFloats src = map read . map (\(a:as) -> case a of '.' -> '0':a:as; '-' -> a:'0':as; _ -> a:as) $ src ::[Float]
 
 initParseIO :: [String] -> ScreenBuffer -> Output Float -> (Float,Float,Float) -> IO ()
 initParseIO ls sbuf out col = parseIO ls sbuf $ (initRenderable defaultArea out col :: Renderable ListMatrix Float)
 	where 
 		defaultArea = Area {xRange = (0,0), yRange = (0,0)}
 
-parsers :: (Matrix m) => Map String ([String] -> Renderable m Float -> Renderable m Float)
-parsers = ML.union formatParsers $ ML.union graphicalParsers $ transformationParsers
-	where	
-	formatParsers = ML.fromList [
-		("screen",parseScreen),
-		("pixels",parsePixels)
-		]
-	graphicalParsers = ML.fromList [
-		("line",parseLine),
-		("box-t",parseBoxT),
-		("sphere",parseSphere),
-		("triangle",parseTriangle)
-		]
-	transformationParsers = ML.fromList [
-		("identity",parseIdentity),
-		("move",parseMove),
-		("scale",parseScale),
-		("rotate-x",parseRotX),
-		("rotate-y",parseRotY),
-		("rotate-z",parseRotZ),
-		("transform",parseTransform)
-		]
 
 parse :: Matrix m => String -> Map String (Sequence a) -> Renderable m Float -> Renderable m Float
 parse str varys buf = let (w:ws) = words str in case ML.lookup w parsers of
@@ -140,28 +115,5 @@ parseIO (l:ls) sbuf buffer@(Renderable scr out col edge mls mtri)
 		cull eye = filter (backFace eye . rows)
 		projCull eye@(ex,ey,ez) = project eye $ cull [ex,ey,ez]
 
-parse :: Matrix m => String -> Renderable m Float -> Renderable m Float
-parse l buffer@(Renderable scr out col edge mls mtri)
-	--Config
-	| w == "screen" = let (xl:yl:xh:yh:_) = readFloats ws in (buffer {_screen = Area {xRange=(xl,xh),yRange=(yl,yh)}})
-	| w == "pixels" = let (x:y:_) = readFloats ws in (buffer {_out = Area {xRange=(0,x),yRange=(0,y)}})
-	--Objects
-	| w == "line" = let (x1:y1:z1:x2:y2:z2:_) = readFloats ws in (buffer {_lineMatrix = (line x1 y1 z1 x2 y2 z2) : mls})
-	| w == "box-t" = let (sx:sy:sz:rx:ry:rz:mx:my:mz:_) = readFloats ws in (buffer {_triangleMatrix = unitCube ++ mtri})
-	| w == "sphere" = let (r:divs:x:y:z:_) = readFloats ws in (buffer {_triangleMatrix = map ((flip matrixProduct) (move x y z)) $ sphereTri r (floor divs) ++ mtri})
-	| w == "triangle" = buffer {_triangleMatrix = (fromList [[1,0,0,1],[0,1,0,1],[0,0,1,1]]) : mtri}
-	--Transformations
-	| w == "identity" = buffer { _edgematrix = identity 4 4}
-	| w == "move" = let (x:y:z:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (move x y z)}
-	| w == "scale" = let (x:y:z:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (scale x y z)}
-	| w == "rotate-x" = let (deg:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (rotateX deg)}
-	| w == "rotate-y" = let (deg:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (rotateY deg)}
-	| w == "rotate-z" = let (deg:_) = readFloats ws in buffer {_edgematrix = matrixProduct edge (rotateZ deg)}
-	| w == "transform" = buffer {_lineMatrix = map ((flip matrixProduct) edge) mls,_triangleMatrix = map ((flip matrixProduct) edge) mtri, _edgematrix = (identity 4 4)}
-	| w == "vary" = buffer
-	| otherwise = buffer
-	where
-		(w:ws) = words l
-		readInts src = map read src ::[Int]
 
 -- Functions
