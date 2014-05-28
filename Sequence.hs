d8de43481d108359fd4cefcbd2eeaebdeaff78d5
hunteriam <hhstuy@gmail.com>
2014-05-28 00:10:25 -0400

    Usability function

--- Sequence.hs
+++ Sequence.hs
@@ -1,10 +1,22 @@
 module Sequence
 (	Sequence(..),
 	currentSequence,
+	seqVal,
 	)
 where
 
-data Sequence a = Anim3D {_frames :: (Int,Int), _values :: (a,a)}
+data Sequence a = Anim3D {_frames :: (Int,Int), _values :: (a,a)} deriving (Show,Eq)
 
 currentSequence :: Int -> Sequence a -> Bool
 currentSequence f (Anim3D (fs,fe) _) = f >= fs && f <= fe
+
+seqVal :: (Num a,Fractional a) => Int -> Sequence a -> a
+seqVal fnum seq@(Anim3D (fs,fe) (vs,ve)) =
+	case f (compare fnum fs) (compare fnum fe) of
+		LT -> vs
+		GT -> ve
+		EQ -> (fromIntegral (fnum - fs) / fromIntegral (fe - fs)) * (ve - vs) + vs
+	where
+		f LT LT = LT
+		f GT GT = GT
+		f _ _ = EQ
