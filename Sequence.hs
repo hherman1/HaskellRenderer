ea3b98196eb475b373c600b63af515dc7e27e708
hunteriam <hhstuy@gmail.com>
2014-06-02 00:24:48 -0400

    Convenience functions

--- Sequence.hs
+++ Sequence.hs
@@ -1,6 +1,8 @@
 module Sequence
 (	Sequence(..),
 	currentSequence,
+	seqsVal,
+	chooseSeq,
 	seqVal,
 	)
 where
@@ -9,6 +11,15 @@ data Sequence a = Anim3D {_frames :: (Int,Int), _values :: (a,a)} deriving (Show
 
 currentSequence :: Int -> Sequence a -> Bool
 currentSequence f (Anim3D (fs,fe) _) = f >= fs && f <= fe
+
+seqsVal :: (Num a, Fractional a) => Int -> [Sequence a] -> a
+seqsVal n = seqVal n . chooseSeq n
+
+chooseSeq :: Int -> [Sequence a] -> Sequence a
+chooseSeq _ [] = error "no sequences to select from"
+chooseSeq n seqs = case filter (currentSequence n) seqs of
+		(s:_) -> s
+		[] -> head seqs
 
 seqVal :: (Num a,Fractional a) => Int -> Sequence a -> a
 seqVal fnum (Anim3D (fs,fe) (vs,ve)) =
