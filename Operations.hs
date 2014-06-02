c5ffa9e2406ce172823c143461f4a093a411df89
hunteriam <hhstuy@gmail.com>
2014-06-02 01:12:11 -0400

    add renderCyclops

--- Operations.hs
+++ Operations.hs
@@ -5,7 +5,13 @@ module Operations (
 import Matrix
 import Matrix3D
 import Objects hiding (sphere)
+import Render
+import OpenGL
+import PPM
 type Tform = (Double,Double,Double)
+
+cull eye = filter (isBackface eye . rows)
+projCull eye@(ex,ey,ez) = project eye . cull [ex,ey,ez]
 
 
 cube :: (Matrix m) => Tform -> Tform -> Tform -> [m Double]
@@ -14,3 +20,12 @@ cube s r m = (flip map) unitCube $ transform (collate [scale s, rotate r, move m
 sphere :: (Matrix m) => Double -> Double -> Tform -> Tform -> Tform -> [m Double]
 sphere rad divs s r m = (flip map) (sphereTri rad (floor divs)) 
 	$ transform (collate [scale s, rotate r, move m])
+
+renderCyclops :: (Matrix m) => Tform -> Renderable m Double -> IO ()
+renderCyclops eye buffer@(Renderable scr out col mls mtri) = do
+	putStrLn "Rendering cyclops"
+	display (toSize out) . bufToPPM out $ (render $ buffer {
+		_col = green, 
+		_lineMatrix = project eye mls,
+		_triangleMatrix = projCull eye mtri
+	} :: [(Int,Int,Color Int)])
